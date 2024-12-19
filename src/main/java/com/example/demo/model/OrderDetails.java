ade3b985e82750560febd86d3f0d0959952ac33e
ethan <ethanreia@gmail.com>
2024-12-19 17:04:52 -0500

    initial commit

--- src/main/java/com/example/demo/model/OrderDetails.java
+++ src/main/java/com/example/demo/model/OrderDetails.java
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.core.mapping.Field;
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 
 @Document
