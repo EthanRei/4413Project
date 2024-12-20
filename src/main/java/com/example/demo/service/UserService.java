fdf6d6c0575fe0204cb428369cd7f8eb490e4f83
ethan <ethanreia@gmail.com>
2024-12-19 19:21:15 -0500

    initial commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -5,15 +5,20 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.demo.exception.UserNotFoundException;
+import com.example.demo.model.Admin;
 import com.example.demo.model.User;
+import com.example.demo.repository.AdminRepository;
 import com.example.demo.repository.UserRepository;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private AdminRepository adminRepository;
     @Autowired
     private CartService cartService;
 
@@ -54,9 +59,44 @@ public class UserService {
 	    return userRepository.findAll();
 
 	}
-	public User updateUser(User user) {
-		
-		return userRepository.save(user);
+	public User updateUser(String customerId, User updatedUser) throws UserNotFoundException {
+		User existingUser = getCustomerById(customerId);
+        // CHECKING if any fields need an update.
+        if (updatedUser.getUsername() != null) {
+            existingUser.setUsername(updatedUser.getUsername());
+        }
+        if (updatedUser.getPassword() != null) {
+            existingUser.setPassword(updatedUser.getPassword());
+        }
+        if (updatedUser.getFirstName() != null) {
+            existingUser.setFirstName(updatedUser.getFirstName());
+        }
+        if (updatedUser.getLastName() != null) {
+            existingUser.setLastName(updatedUser.getLastName());
+        }
+        if (updatedUser.getEmail() != null) {
+            existingUser.setEmail(updatedUser.getEmail());
+        }  
+        if (updatedUser.getCreditCardNumber() != null) {
+            existingUser.setCreditCardNumber(updatedUser.getCreditCardNumber());
+        }
+        if (updatedUser.getAddress() != null) {
+            existingUser.setAddress(updatedUser.getAddress());
+        }
+		return userRepository.save(updatedUser);
 	}
 	
+    public Admin addAdmin(@RequestBody Admin admin) {
+        return adminRepository.save(admin);
+    }
+    
+    
+	public boolean validateAdmin(String adminUsername, String adminPassword) {
+		
+		Optional<Admin> admincheck = adminRepository.findByUsername(adminUsername);
+		
+        System.out.println("Admin found: " + admincheck.isPresent());
+        
+        return admincheck.filter(admin -> admin.getPassword().equals(adminPassword)).isPresent();
+	}
 }
